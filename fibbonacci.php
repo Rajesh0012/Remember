d51fb5718c4c80adfc5306c4aba429bf0e307a9b
Rajesh <rajeshmaurya0012@gmail.com>
2017-12-23 23:06:53 +0530

    new updation

--- fibbonacci.php
+++ fibbonacci.php
@@ -1,8 +1,20 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Created by Rajesh.
+ * Date: 23-Dec-17
+ * Time: 10:10 PM
  */
 
+ $a=1;
+ $b=2;
+echo $a.'<br>'.$b.'<br>';
+for($i=1;$i<=12;$i++){
+
+    $c=$a+$b;
+    $a=$b;
+    $b=$c;
+    echo $c.'<br/>';
+
+
+
+}
